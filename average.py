fa2c21f394ff0846a51fcbe4ff3b6a44caaa934b
arakona <40288919+arakona@users.noreply.github.com>
2018-06-19 17:49:09 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,4 @@
+#average of five nos
+a,b,c,d,e=eval(input("enter five numbers\n"))
+print("average of five nos is:")
+print((a+b+c+d+e)/5)
---
+++ python.py
@@ -0,0 +1,4 @@
+c=eval(input("enter temperature in degree celsius\n"))
+f=(9/5)*c+32
+print("temp in fahrenheit is: ",f)
+
